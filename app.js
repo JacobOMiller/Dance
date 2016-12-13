dfddc241dad52c5a868583165a413dea3b789b34
Jacob Miller <jacob.ohara.miller@gmail.com>
2016-12-13 11:12:59 -0500

    adds alert feature

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 function pointLeft() {
   console.log("point left");
+  alert("Point left");
 }
 function pointRight() {
   console.log("point right");
+  alert("point right");
 }
 function stepLeft(){
   console.log("step Left");
+  alert("step left");
 }
 function stepRight(){
   console.log("step Right");
+  alert("step right");
 }
 
 for (var i = 0; i < 2; i++) {
